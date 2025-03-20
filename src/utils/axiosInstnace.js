933fe101d71d637baa3e09d5ae55e3b894da2dbd
nue-os <kimsoeun0108@gmail.com>
2025-03-20 13:19:14 +0900

    chore: axiosInstance 설정

---
+++ src/utils/axiosInstnace.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 요청 인터셉터 설정
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = sessionStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export default axiosInstance;
